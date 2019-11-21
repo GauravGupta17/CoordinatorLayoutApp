4f0bfb898e2af02bc62b41c310579e8c03089e8c
GauravGupta17 <Gauravkld007@gmail.com>
2019-11-21 12:19:26 +0530

    "Full Co ordinator View Implementation"

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.fragments
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -14,24 +17,73 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
 
 
         fabSnackbar.setOnClickListener {
 
-         Snackbar.make(it,"Its SnackBar",Snackbar.LENGTH_LONG).setAction("Action") {
+              val intent = Intent(this,ScrollViewActivity::class.java)
 
-             newToast("New Toast From Extension",Toast.LENGTH_LONG)
 
-         }.show()
+
+
+
+            startActivity(intent)
+
+
 
         }
+
+        fabSnackbar.setOnLongClickListener{
+
+            Snackbar.make(it, "Its SnackBar", Snackbar.LENGTH_LONG).setAction("Action") {
+
+                newToast("New Toast From Extension", Toast.LENGTH_LONG)
+
+            }.show()
+
+            true
+        }
+
+
+
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+        return when (item.itemId) {
+            R.id.menuSettings -> true
+            else -> super.onOptionsItemSelected(item)
+
+
+        }
+
+
+    }
+
+
+    fun AppCompatActivity.newToast(text: String, length: Int = Toast.LENGTH_SHORT) {
+
+        Toast.makeText(this, text, length).show()
+
+    }
+
+
 }
 
 
-fun AppCompatActivity.newToast(  text:String,length:Int = Toast.LENGTH_SHORT){
 
-    Toast.makeText(this, text, length).show()
 
-}
+
+
+
+
+
---
+++ app/src/main/java/com/example/fragments/ScrollViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll_view)
+
+    }
+}
